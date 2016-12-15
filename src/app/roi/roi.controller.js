699373ae05fb341b3f12ea6532787ece20d85f74
Sourav Mondal <sourav@devathon.com>
2016-12-15 16:31:17 +0530

    [ADD] ROI calculator function

--- src/app/roi/roi.controller.js
+++ src/app/roi/roi.controller.js
@@ -6,16 +6,46 @@
         .controller('RoiController', RoiController);
 
     /** @ngInject */
-    function RoiController($log) {
+    function RoiController($log, $scope) {
         var vm = this;
+
+        vm.factors = {
+            activeUserFactor: 0.7,
+            conversionFactor: 0.2,
+            discountFactor: 0.2,
+            campaignsPerMonth: 1
+        };
 
         vm.customers = 0;
         vm.transactionValue = 0;
         vm.visitorsPerMonth = 0;
         vm.stores = 0;
+        vm.roi = 0;
+
+        vm.calculateROI = calculateROI;
+
+        function calculateROI() {
+            return (vm.customers * vm.transactionValue * vm.stores * vm.factors.activeUserFactor *
+                vm.factors.conversionFactor * (1 - vm.factors.discountFactor) * vm.factors.campaignsPerMonth);
+        }
 
         $log.info('ROI Controller');
 
-        $watchCollection()
+        $scope.$watchGroup([
+            function () {
+                return vm.customers;
+            }, function () {
+                return vm.transactionValue;
+            }, function () {
+                return vm.visitorsPerMonth;
+            }, function () {
+                return vm.stores;
+            }],
+            function changeDetector(newVal, oldVal) {
+                $log.info(newVal, oldVal);
+
+                vm.roi = vm.calculateROI();
+            }
+        )
     }
 })();
